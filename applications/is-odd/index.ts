af36972bc535569c123f6145985397fc37380b3f
XeniaChe <xeniacserkun@gmail.com>
2023-08-02 13:09:40 +0200

    Nodemon

--- applications/is-odd/index.ts
+++ applications/is-odd/index.ts
@@ -1,5 +1,6 @@
 import { isEven } from 'is-even';
-
+import { test } from './src/test';
 const isOdd = (num: number) => !isEven(num);
 
-console.log('TURBOREPO: ' + isOdd(5));
+console.log('TURBOREPO: ' + isOdd(4));
+console.log('TEST: ' + test);
